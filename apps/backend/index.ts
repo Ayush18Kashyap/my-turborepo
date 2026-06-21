db5240c40ef85ae3e30a467602a07e2046f3ec53
Ayush18Kashyap <130841684+Ayush18Kashyap@users.noreply.github.com>
2026-06-21 23:23:21 +0530

    linkedin scrapper removed

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -5,24 +5,28 @@ import axios from 'axios';
 const app = express();
 app.use(express.json());
 
-app.post('/api/v1/pre-interview', (req, res) => {
-    const {success,data} = PreInterviewBody.safeParse(req.body); // here two variables are returned, success is a boolean indicating if the parsing was successful, and data is the parsed data if successful, or an error object if not.
+app.post('/api/v1/pre-interview', async (req, res) => {
+    const { success, data } = PreInterviewBody.safeParse(req.body); // here two variables are returned, success is a boolean indicating if the parsing was successful, and data is the parsed data if successful, or an error object if not.
 
     if (!success) {
-        return res.status(411).json({ message : 'Incorrect body'});
+        return res.status(411).json({ message: 'Incorrect body' });
     }
 
 
     const githubUrl = data.github.endsWith('/') ? data.github.slice(0, -1) : data.github;
-    const linkedinUrl = data.linkedin.endsWith('/') ? data.linkedin.slice(0, -1) : data.linkedin;
 
     const githubUsername = githubUrl.split('/').pop();
-    const linkedinUsername = linkedinUrl.split('/').pop();
 
     const githubRepos = await axios.get(`https://api.github.com/users/${githubUsername}/repos`);
-    const linkedinProfile = await axios.get(`https://api.linkedin.com/v2/profile/${linkedinUsername}`);
 
-};
+
+    const filteredRepos = githubRepos.data.map((repo: any) => ({
+        description: repo.description,
+        name: repo.name,
+        fullName: repo.full_name,
+        starcount: repo.stargazers_count
+    }));
+});
 
 
 
--- packages/ui/src/Form.tsx
+++ packages/ui/src/Form.tsx
@@ -4,26 +4,21 @@ import axios from "axios";
 
 export function Form() {
 
-    const [linkedin, setLinkedin] = useState('');
     const [github, setGithub] = useState('');
 
     async function onSubmit() {
-        if (!linkedin || !github) {
+        if (!github) {
             alert("Please fill in all fields.");
             return;
         }
 
         await axios.post(`${BACKEND_URL}/api/v1/pre-interview`,{
-            linkedin,
             github
         })
     }
     return (
             <div>
                 <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">AI Interviewer</h2>
-                <div>
-                    <input type="text" placeholder="Linkedin URL" onChange={(e) => setLinkedin(e.target.value)} className="p-4" />
-                </div>
                 <div>
                     <input type="text" placeholder="GitHub URL" onChange={(e) => setGithub(e.target.value)} className="p-4" />
                 </div>
